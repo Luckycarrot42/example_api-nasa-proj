034d841c1b388a430e871c85b4b1eb481124667b
Luckycarrot42 <eicarriemontecinos@gmail.com>
2020-09-05 19:20:57 -0400

    Comentario

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBLd-hgj52ZUdVQ7IjNdRzA7Dgql5zaQZ8",
+  authDomain: "apinasa-b87d9.firebaseapp.com",
+  databaseURL: "https://apinasa-b87d9.firebaseio.com",
+  projectId: "apinasa-b87d9",
+  storageBucket: "apinasa-b87d9.appspot.com",
+  messagingSenderId: "934345329344",
+  appId: "1:934345329344:web:a35f2066b6a62300147b25"
+}
